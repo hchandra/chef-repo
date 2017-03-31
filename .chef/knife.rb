83915ad3264f3d1951afafe3f7ff36c68fe52753
Hiren Chandratre <hiren.chandratre@gmail.com>
2017-03-31 10:12:59 -0600

    initial commit

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "hchandra"
+client_key               "#{current_dir}/hchandra.pem"
+chef_server_url          "https://api.chef.io/organizations/barcap"
+cookbook_path            ["#{current_dir}/../cookbooks"]
